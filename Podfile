7e8a449d4d43d92839443e8977300a7dcbca733d
micchyboy1023 <masamichi1023@gmail.com>
2016-09-21 00:43:30 +0900

    Initial commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+use_frameworks!
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+
+target 'ReSwiftCoreData' do
+  pod 'ReSwift'
+end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings['SWIFT_VERSION'] = '3.0'
+    end
+  end
+end
+
---
+++ ReSwiftCoreData/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  ReSwiftCoreData
+//
+//  Created by UetaMasamichi on 9/21/16.
+//  Copyright © 2016 MasamichiUeta. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
