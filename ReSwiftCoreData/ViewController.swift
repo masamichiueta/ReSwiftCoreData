d20d4bf24d67d17dcb384e82c2461501980052e9
micchyboy1023 <masamichi1023@gmail.com>
2016-09-21 00:30:27 +0900

    Initial Commit

---
+++ ReSwiftCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReSwiftCoreData
+//
+//  Created by UetaMasamichi on 9/21/16.
+//  Copyright © 2016 MasamichiUeta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
